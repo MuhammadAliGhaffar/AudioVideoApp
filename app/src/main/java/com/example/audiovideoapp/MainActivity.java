657c080540a0a4064fafc68baa2c84f09f55ceca
MuhammadAliGhaffar <alibrohi999@gmail.com>
2020-08-21 14:54:57 +0500

    Adding mp3 file in raw folder and add some functionality adding another way to create method using switch case + adding seekbar voloume controller

--- app/src/main/java/com/example/audiovideoapp/MainActivity.java
+++ app/src/main/java/com/example/audiovideoapp/MainActivity.java
@@ -1,19 +1,27 @@
 package com.example.audiovideoapp;
 
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.MediaController;
+import android.widget.SeekBar;
 import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    //UI Components
+
     private VideoView myvideoView;
-    private Button btnPlayVideo;
-    MediaController mediaController;
+    private Button btnPlayVideo,btnPlayMusic,btnPauseMusic;
+    private MediaController mediaController;
+    private MediaPlayer playerAudio;
+    private SeekBar mSeekBarVoume;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,20 +30,50 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         myvideoView=findViewById(R.id.myvideoView);
         btnPlayVideo=findViewById(R.id.btnPlayVideo);
+        btnPlayMusic=findViewById(R.id.btnPlayMusic);
+        btnPauseMusic=findViewById(R.id.btnPauseMusic);
+        mSeekBarVoume=findViewById(R.id.seekBarVolume);
+
 
         btnPlayVideo.setOnClickListener(MainActivity.this);
+        btnPlayMusic.setOnClickListener(MainActivity.this);
+        btnPauseMusic.setOnClickListener(MainActivity.this);
+
         mediaController = new MediaController(MainActivity.this);
+
+        playerAudio= MediaPlayer.create(this, R.raw.music);
+
+
 
     }
 
     @Override
-    public void onClick(View view) {
+    public void onClick(View buttonView) {
 
 
-        myvideoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.mow));
-        myvideoView.setMediaController(mediaController);
-        mediaController.setAnchorView(myvideoView);
+        switch (buttonView.getId()){
 
-        myvideoView.start();
+            case R.id.btnPlayVideo:
+                myvideoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.mow));
+                myvideoView.setMediaController(mediaController);
+                mediaController.setAnchorView(myvideoView);
+                myvideoView.start();
+                break;
+
+            case R.id.btnPlayMusic:
+
+                playerAudio.start();
+                Log.i("TAG","Play Music Button is Tapped");
+                break;
+
+            case R.id.btnPauseMusic:
+
+                playerAudio.pause();
+                Log.i("TAG","Pause Music Button is Tapped");
+                break;
+        }
+
+
+
     }
 }
